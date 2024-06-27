514d8060109d8b4aba985bb3c0d3110cddd98d00
Kirk Enbysk <kirkenbysk@gmail.com>
2024-06-27 10:32:00 -0400

    test navigator geolocation method

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,23 @@
+import axios from 'axios'
 import React from 'react'
 
 const HomePage = () => {
+  
+  // const findLocation = async () => {
+  //   res = await axios
+  // }
+
+// console.log(window.navigator);
+
+  const newClip = () => {navigator.geolocation.getCurrentPosition((position) => console.log(position));
+
+  }
+
   return (
+    <>
     <div>HomePage</div>
+    <a onClick={newClip}>wooooo</a>
+  </>
   )
 }
 
