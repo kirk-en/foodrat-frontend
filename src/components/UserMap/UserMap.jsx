99e63dc161f7763e4e967dd19648db37c8cefe5b
Kirk Enbysk <kirkenbysk@gmail.com>
2024-07-03 14:28:06 -0400

    insert html test into advanced market, destructure incoming inspection data

--- src/components/UserMap/UserMap.jsx
+++ src/components/UserMap/UserMap.jsx
@@ -1,7 +1,14 @@
-import { APIProvider, Map, useMap } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map,
+  useMap,
+  AdvancedMarker,
+} from "@vis.gl/react-google-maps";
 import "./UserMap.scss";
 import { useCallback, useEffect, useRef, useState } from "react";
 import axios from "axios";
+import markerImageA from "../../assets/letter-grades/grade-a.svg";
+import markerImageB from "../../assets/letter-grades/grade-b.svg";
 
 // The debouncer will prevent an API request being sent to NYC Open Data
 // until the map has stopped moving for 1 second.
@@ -39,18 +46,14 @@ const UserMap = ({ location }) => {
   debouncer(
     async () => {
       console.log("GET request sent to NYC OpenData");
-      const res = await axios.get(
+      const { data } = await axios.get(
         `https://data.cityofnewyork.us/resource/43nn-pn8j.json?$WHERE=latitude < ${bounds.north} AND latitude > ${bounds.south} AND longitude < ${bounds.east} AND longitude > ${bounds.west}`
       );
-      console.log(res.data);
+      console.log(data);
     },
     1000,
     [bounds]
   );
-
-  const loadCheck = (map) => {
-    console.log("onLoad happened", map);
-  };
 
   return (
     <>
@@ -60,14 +63,31 @@ const UserMap = ({ location }) => {
         // could use this onload to grab data from NYC database
       >
         <Map
-          // style={{ width: "100%", height: "100%" }}
+          mapId="foodrat"
+          style={{ width: "100%", height: "100%" }}
           className="map"
           defaultCenter={{ lat: location.latitude, lng: location.longitude }}
           defaultZoom={18}
           gestureHandling={"greedy"}
           disableDefaultUI={true}
           onCameraChanged={handleCameraChange}
-        />
+        >
+          <AdvancedMarker
+            title="Store 1"
+            position={{ lat: 40.724261421332, lng: -73.9966159052 }}
+            onClick={() => console.log("marker clicked")}
+          >
+            <p>This is a store</p>
+            <img src={markerImageA} width={32} height={32} />
+          </AdvancedMarker>
+          <AdvancedMarker
+            title="Store 1"
+            position={{ lat: 40.722979589862, lng: -73.995843921175 }}
+            onClick={() => console.log("marker clicked")}
+          >
+            <img src={markerImageB} width={32} height={32} />
+          </AdvancedMarker>
+        </Map>
       </APIProvider>
     </>
   );
