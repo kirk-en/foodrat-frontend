b3312088d0fb46ba6507e5bf0779b36b8498d6a7
Kirk Enbysk <kirkenbysk@gmail.com>
2024-07-03 00:37:46 -0400

    return array of restaurant violation in the users radius from NYC Open Data API

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,13 +8,30 @@ import Header from "../../components/Header/Header";
 
 import "./HomePage.scss";
 
+// set this location if the user declines to use location sharing
+const defaultLocation = {
+  latitude: 40.690454399970044,
+  longitude: -74.04516135490462,
+  altitude: null,
+  accuracy: 40,
+  altitudeAccuracy: null,
+  heading: null,
+  speed: null,
+};
+
 const HomePage = () => {
   const [location, setLocation] = useState();
 
   const getLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLocation(position.coords);
-    });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log("position:", position);
+        setLocation(position.coords);
+      },
+      () => {
+        setLocation(defaultLocation);
+      }
+    );
   };
 
   useEffect(() => {
